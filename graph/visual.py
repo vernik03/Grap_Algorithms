85a56fa2c341666d741527c32a316ca27ae38cc4
vernik03 <vernik03@gmail.com>
2022-02-18 02:20:52 +0200

    need more threads

--- graph/visual.py
+++ graph/visual.py
@@ -23,24 +23,21 @@ def get_matrix_triad(coo_matrix , data=False):
 	temp = np.vstack((coo_matrix.row , coo_matrix.col , coo_matrix.data)).transpose()
 	return temp.tolist()
 
-matrix = np.genfromtxt("matrix.txt", delimiter=' ')
-edags = get_matrix_triad(matrix)
-G = nx.Graph()
-H = nx.path_graph(matrix.shape[0]) 
-G.add_nodes_from(H)
-G.add_weighted_edges_from(edags)
-colors = np.arange(matrix.shape[0])
-
 def PrintMatrix():   
     nx.draw(G,pos=nx.spring_layout(G),node_color="black")
+    print('!')
     plt.show()
+    
 
 def ReadFile(name): #!!! takes 1 positional argument but 11 were given (почему?)
     name = "command.txt"
     while True:
-        f = open(name, 'r')        
-        if f.readline() == "new":
+        f = open(name, 'r')  
+        print('!')      
+        str = f.readline()
+        if str == "new\n":
             f.close()
+            print('!!!') 
             th_paint.start()
         f.close()
         time.sleep(5)
@@ -49,23 +46,33 @@ def ReadFile(name): #!!! takes 1 positional argument but 11 were given (поче
 def PaintGraph(name):    
     f = open(name, 'r')    
     if f.readline() == "new":
-        if  f.readline() == "DepthFirstSearch":       
+        if  f.readline() == "DepthFirstSearch\n":       
             nx.draw(G,pos=nx.spring_layout(G),node_color="red")
-        elif f.readline() == "BreadthFirstSearch":   
+        elif f.readline() == "BreadthFirstSearch\n":   
             nx.draw(G,pos=nx.spring_layout(G),node_color="green")
-        elif f.readline() == "PrimsAlgorithm":   
+        elif f.readline() == "PrimsAlgorithm\n":   
             nx.draw(G,pos=nx.spring_layout(G),node_color="blue")
-        elif f.readline() == "DijkstraAlgorithm":   
-            nx.draw(G,pos=nx.spring_layout(G),node_color="yellow")
+        elif f.readline() == "DijkstraAlgorithm\n":   
+            nx.draw(G,pos=nx.spring_layout(G),node_color="yellow")        
         plt.show()
         f.close()
         fw = open(name, 'w')
         fw.write('painted')
-    
+
+matrix = np.genfromtxt("matrix.txt", delimiter=' ')
+edags = get_matrix_triad(matrix)
+G = nx.Graph()
+H = nx.path_graph(matrix.shape[0]) 
+G.add_nodes_from(H)
+G.add_weighted_edges_from(edags)
+colors = np.arange(matrix.shape[0])
+
+print('~')
 s = "c"
-th_matrix = Thread(target=PrintMatrix)
+th_matrix = Thread(target=PrintMatrix, daemon=True)
 th_read = Thread(target=ReadFile, args=(s))
-th_paint = Thread(target=PaintGraph, args=(s))
+th_paint = Thread(target=PaintGraph, args=(s),daemon=True)
+print('~~')
 th_matrix.start()
 th_read.start()
 
