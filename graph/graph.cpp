e9553cbc20b0317134abc09ced5df880f60a2652
vernik03 <vernik03@gmail.com>
2022-02-20 03:06:06 +0200

    6

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -195,7 +195,7 @@ int h(int current, const vector<vector<int>>& graph, unordered_map<int, int> cam
 	return h;
 }
 
-void AStar(const int& count_elems, const vector<vector<int>>& graph) {
+void AStarAlgorithm(const int& count_elems, const vector<vector<int>>& graph) {
 
 	int start, goal;
 	cout << "A-star algorithm, enter first node number: ";
@@ -220,7 +220,6 @@ void AStar(const int& count_elems, const vector<vector<int>>& graph) {
 		f_score[i] = INT_MAX;
 	}
 	f_score[start] = h(start, graph, came_from);
-	//came_from[start] = start;
 
 	int current = 0;
 	while (!open_set.empty()) {
@@ -252,12 +251,12 @@ void AStar(const int& count_elems, const vector<vector<int>>& graph) {
 			}
 		}
 	}
-	//cout << start << " ";
 	std::vector<int> total_path = { current };
 	while (current != start) {
 		current = came_from[current];
 		total_path.push_back(current);
 	}
+	reverse(total_path.begin(), total_path.end());
 	for (auto elem : total_path) {
 		std::cout << elem + 1 << " ";
 	}
@@ -265,12 +264,141 @@ void AStar(const int& count_elems, const vector<vector<int>>& graph) {
 	cout << endl;
 }
 
+vector<int> AStarAlgorithmVector(const int& count_elems, const vector<vector<int>>& graph, int start, int goal) {
+	unordered_set<int> open_set;
+	open_set.insert(start);
+	unordered_map<int, int> came_from;
+	map<int, int> g_score;
+	for (int i = 0; i < count_elems; i++)
+	{
+		g_score[i] = INT_MAX;
+	}
+	g_score[start] = 0;
+	map<int, double> f_score;
+	for (int i = 0; i < count_elems; i++)
+	{
+		f_score[i] = INT_MAX;
+	}
+	f_score[start] = h(start, graph, came_from);
+
+	int current = 0;
+	while (!open_set.empty()) {
+		current = *(--(open_set.end()));
+		for (auto elem : open_set)
+		{
+			if (f_score.at(elem) < f_score.at(current) && elem != start)
+			{
+				current = elem;
+			}
+		}
+		if (current == goal) {
+			break;
+		}
+		open_set.extract(current);
+		for (int i = 0; i < count_elems; i++) {
+			if (graph[current][i])
+			{
+				int tentative_g_score = g_score[current] + graph[current][i];
+				if (tentative_g_score < g_score[i]) {
+					came_from[i] = current;
+					g_score[i] = tentative_g_score;
+					f_score[i] = tentative_g_score + h(i, graph, came_from);
+					if (!open_set.count(i))
+					{
+						open_set.insert(i);
+					}
+				}
+			}
+		}
+	}
+	std::vector<int> total_path = { current };
+	while (current != start) {
+		current = came_from[current];
+		total_path.push_back(current);
+	}
+	reverse(total_path.begin(), total_path.end());
+	return total_path;
+}
+
+
+void FordFulkersonAlgorithm(const int& count_elems, const vector<vector<int>>& graph) {
+	int pathCount = 0;
+	int start, goal;
+	cout << "Ford-Fulkerson algorithm, enter first node number: ";
+	cin >> start;
+	start--;
+	cout << "Enter last node number: ";
+	cin >> goal;
+	goal--;
+	vector<vector<int>> flow(count_elems, { 0, 0 });
+	vector<vector<int>> graph_temp = graph;
+	int max_flow = 0;
+	set<int> visited_really;
+	vector<int> visited;
+	map<int, int> parent;
+	while (true) {
+		visited = AStarAlgorithmVector(count_elems, graph, start, goal);
+		for (int i = 0; i < visited.size()-1; i++)
+		{
+			parent[visited[i + 1]] = visited[i];
+		}
+		int flag = 0;
+		for (int i = 0; i < visited.size(); i++)
+		{
+			if (visited_really.count(visited[i])>0)
+			{
+				flag++;
+			}
+			else
+			{
+				visited_really.insert(visited[i]);
+			}			
+		}
+		if (flag == visited.size())
+		{
+			break;
+		}
+
+		int path_flow = INT_MAX;
+		int s = goal;
+		while (s!=start)
+		{
+			path_flow = min(path_flow, graph_temp[parent[s]][s]);
+			s = parent[s];
+		}
+		max_flow += path_flow;
+		int v = goal;
+		while (v != start)
+		{
+			int u = parent[v];
+			graph_temp[u][v] -= path_flow;
+			graph_temp[v][u] += path_flow;
+			v = parent[v];
+		}
+		/*
+		int delta = 10000;
+		for (int u = 0; u < visited.size(); u++) // Найти минимальный поток в сети
+		{
+			delta = min(delta, (graph[visited[u]][u] - flow[visited[u]][u]));
+		}
+		pathCount++;
+		for (int u = 0; u < visited.size(); u++) // По алгоритму Форда-Фалкерсона 
+		{
+			u--;
+			flow[visited[u]][u] += delta;
+			flow[u][visited[u]] -= delta;
+		}
+		max_flow += delta;
+		*/
+	}
+	cout << max_flow << endl;
+}
 
 int main()
 {
     vector<vector<int>> graph;
 	int count_elems = 0;
-	bool elem;
+	int elem;
 	ifstream in("matrix.txt");
 	while (!in.eof())
 	{
@@ -298,12 +426,13 @@ int main()
 			BreadthFirstSearch(count_elems, graph);
 			PrimsAlgorithm(count_elems, graph);
 			DijkstraAlgorithm(count_elems, graph);
-			AStar(count_elems, graph);
+			AStarAlgorithm(count_elems, graph);
+			FordFulkersonAlgorithm(count_elems, graph);
 		});
 
 	thread t2([&]()
 		{
-			system("python visual.py");			
+			//system("python visual.py");			
 		});
 
 	t1.join();
